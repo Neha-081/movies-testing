d218fdfa2a96c6aeb25f8322316a04020e62c293
Neha <nehaprajapati1150@gmail.com>
2021-12-22 22:41:09 +0530

    fake DOM events

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,18 +1,21 @@
-it('It shows an autocomplete',()=>{
-   createAutoComplete({
-       root:document.querySelector('#target'),
-       fetchData(){
-           return [
-               {Title:'Avengers'},
-               {Title:'Not Avengers'},
-               {Title:'Some other Movie'},
-           ];
-       },
-       renderOption(movie){
-           return movie.Title;
-       }
-   });
+beforeEach(()=>{      //hook provided by mocha globally
+    document.querySelector('#target').innerHTML='';
+    createAutoComplete({
+        root:document.querySelector('#target'),
+        fetchData(){
+            return [
+                {Title:'Avengers'},
+                {Title:'Not Avengers'},
+                {Title:'Some other Movie'},
+            ];
+        },
+        renderOption(movie){
+            return movie.Title;
+        }
+})
+});
 
+it('Dropdown starts close',()=>{
    //dropdown
    const dropdown=document.querySelector('.dropdown');
    
@@ -20,3 +23,14 @@ it('It shows an autocomplete',()=>{
    
 
 });
+
+it('After searching dropdown opens up',()=>{
+  const input = document.querySelector('input');
+  input.value='avengers';
+  input.dispatchEvent(new Event ('input'))
+
+  const dropdown=document.querySelector('.dropdown');
+   
+   expect(dropdown.className).to.include('is-active') 
+
+})
